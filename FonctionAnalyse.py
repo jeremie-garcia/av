7ae47b08bbb4b0c3d725bd497f431efa1a57f21e
Maxence Berry <maxencerun@gmail.com>
2019-12-30 10:32:18 +0100

    Correctif de FonctionAnalyse
    avAnalyse contient des test en name==main et avAnalyse est une version dictionnarisée du signal

--- FonctionAnalyse.py
+++ FonctionAnalyse.py
@@ -5,29 +5,39 @@ def ListeNormalisée(L):
     minimum = (min(L))
     n = len(L)
     F = [] #on initialise la liste finale
-    for i in range(L):
+    for i in range(n):
         F.append((L[i] - minimum) / (maximum - minimum)) #normalise entre 0 et 1
     return F
 #
 def PasseBas(L, fc):
     '''L doit être une liste où chaque instant i contient un dictionnaire avec une clé freq pour fréquence et rms
-    fc est la fréquence de coupure et on donne un rms nul à toute fréquence supérieure'''
+    fc est la fréquence de coupure et on donne un rms nul à toute fréquence supérieure sans détruire L'''
     n = len(L)
     F = [] #on intialise la liste finale
     for i in range(n):
         F.append(L[i])
         if L[i]['freq'] > fc :
-            F[i]['rms'] == 0 #on annule la puissance
+            F[i]['rms'] = 0 #on annule la puissance
     return F
 #
 def PasseHaut(L, fc):
     '''L doit être une liste où chaque instant i contient un dictionnaire avec une clé freq pour fréquence et rms
-    fc est la fréquence de coupure et on donne un rms nul à toute fréquence inférieur'''
+    fc est la fréquence de coupure et on donne un rms nul à toute fréquence inférieur sans détruire L'''
     n = len(L)
     F = [] #on intialise la liste finale
     for i in range(n):
         F.append(L[i])
         if L[i]['freq'] < fc :
-            F[i]['rms'] == 0 #on annule la puissance
+            F[i]['rms'] = 0 #on annule la puissance
     return F
 #
+if __name__ == '__main__':
+    L = [1, 25, 44, 56, 75, 98, 46, 57]
+    M = ListeNormalisée(L)
+    print(M)
+    T = [{'rms' : 1, 'freq' : 100 * i} for i in range(10)] #On créé une liste de fréq allant de 0 à 900
+    print('T=',T)
+    S = PasseBas(T, 500)
+    print('S=',S)
+    S = PasseHaut(T, 500)
+    print('S=',S)
